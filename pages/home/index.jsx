4b5ea208e656ab20e08dc01471db72fc7fd2bd11
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-01 00:08:35 +0300

    home

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,9 +1,29 @@
+import About from '@/components/About'
+import Campaigns from '@/components/Campaigns'
+import Carousel from '@/components/Carousel'
+import Customers from '@/components/customers/Customers'
+import MenuWrapper from '@/components/product/MenuWrapper'
+import Reservation from '@/components/Reservation'
 import React from 'react'
 
 const Index = () => {
   return (
-    <div className='text-red-600 font-dancing text-center text-3xl'>Feane</div>
+    <>
+      <Carousel />
+      <Campaigns />
+      <MenuWrapper />
+      <About />
+      <Reservation />
+      <Customers />
+    </>
   )
 }
 
 export default Index
+
+
+/*
+Bu dosya, ana sayfa içeriğini oluşturuyor. Slider, kampanyalar, menü, hakkımızda
+gibi bileşenleri gösteriyor.
+Burası sayfanın UI kısmını oluşturuyor ama <title> gibi <head> içeriği burada tanımlanmamış.
+ */
