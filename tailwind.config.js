ce83a604e7e2473d2d6085b09ef1fa5a0934ad33
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-01 00:01:30 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1170px",
+          "2xl": "1170px",
+        },
+      },
       colors: {
         primary: "#ffbe33",
-        secondary: "#222831"
+        secondary: "#222831",
+        danger: "#ff0000",
+        success: "#00ff00",
       },
       fontFamily: {
-        dancing: ["Dancing Script", "cursive"]
-      }
+        dancing: ["Dancing Script", "cursive"],
+        sans: ["Open Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
